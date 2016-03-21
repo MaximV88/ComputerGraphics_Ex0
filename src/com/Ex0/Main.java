119d23a5810a27541ee99cc7ed99faafedda500c
Maxim Vainshtein <maximv88@gmail.com>
2016-03-21 21:31:07 +0200

    Added loading and saving of scene

--- src/com/Ex0/Main.java
+++ src/com/Ex0/Main.java
@@ -1,12 +1,23 @@
 package com.Ex0;
 
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
+import javax.swing.JFileChooser;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.charset.StandardCharsets;
+import java.io.BufferedReader;
+import java.io.StringReader;
 
 public class Main {
 
@@ -30,26 +41,27 @@ public class Main {
 
 }
 
-class MyCanvas extends Canvas implements MouseListener,  MouseMotionListener {
+class MyCanvas extends Canvas implements MouseListener, MouseMotionListener, KeyListener {
 
     private static final long serialVersionUID = 1L;
 
     /** The manager that handles the polygon/polyline creation and storage. */
-    private PolygonManager manager = new PolygonManager();
+    private Scene scene = new Scene();
 
     public MyCanvas() {
 
         setSize(600, 480);
         addMouseListener(this);
         addMouseMotionListener(this);
+        addKeyListener(this);
 
     }
 
     @Override
     public void paint(Graphics g) {
 
-        drawPolyline(g, manager.getPolyline());
-        drawPolygons(g, manager.getPolygons());
+        drawPolyline(g, scene.getPolyline());
+        drawPolygons(g, scene.getPolygons());
 
     }
 
@@ -97,8 +109,8 @@ class MyCanvas extends Canvas implements MouseListener,  MouseMotionListener {
 
     @Override
     public void mouseReleased(MouseEvent e) {
-        manager.setPolylineSection(e.getPoint());
-        manager.endPolylineSection();
+        scene.setPolylineSection(e.getPoint());
+        scene.endPolylineSection();
         this.repaint();
     }
 
@@ -112,7 +124,7 @@ class MyCanvas extends Canvas implements MouseListener,  MouseMotionListener {
 
     @Override
     public void mousePressed(MouseEvent e) {
-        manager.setPolylineSection(e.getPoint());
+        scene.setPolylineSection(e.getPoint());
         this.repaint();
     }
 
@@ -126,16 +138,259 @@ class MyCanvas extends Canvas implements MouseListener,  MouseMotionListener {
 
     @Override
     public void mouseDragged(MouseEvent e) {
-        manager.setPolylineSection(e.getPoint());
+        scene.setPolylineSection(e.getPoint());
         this.repaint();
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+        switch (e.getKeyCode()) {
+
+            //If L key was hit, we load new *.scn file.
+            case KeyEvent.VK_L: {
+
+                SceneManager manager = new SceneManager();
+                Scene newScene = manager.loadSceneWithMenu();
+
+                if (newScene != null) {
+
+                    scene = newScene;
+
+                    //Redraw the loaded scene to update canvas
+                    this.repaint();
+
+                }
+
+                break;
+            }
+
+            //If S key was hit, save the scene
+            case KeyEvent.VK_S: {
+
+                SceneManager manager = new SceneManager();
+                manager.saveSceneWithMenu(scene);
+
+                break;
+            }
+            case KeyEvent.VK_F:
+
+                break;
+
+            default: //Do nothing
+        }
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // TODO Auto-generated method stub
+        ///Nothing to implement here.
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // TODO Auto-generated method stub
+        //Nothing to implement here.
+    }
+
+
+}
+
+class SceneManager {
+
+    /**
+     * Saves a Scene object with a file interface to
+     * specify the file name and path.
+     * @param scene The Scene to save.
+     */
+    public void saveSceneWithMenu(Scene scene) {
+
+        //Open file chooser to load a new scene
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setCurrentDirectory(new File(System.getProperty("user.dir")));
+
+        switch (fileChooser.showSaveDialog(fileChooser)) {
+            case JFileChooser.APPROVE_OPTION: {
+
+                    saveScene(scene, fileChooser.getSelectedFile());
+                    break;
+            }
+            default: //Do nothing
+        }
+
+    }
+
+    /**
+     * Saves a Scene into a file.
+     * @param scene The scene to save.
+     * @param file The file to save the scene into.
+     */
+    public void saveScene(Scene scene, File file) {
+
+        try {
+
+            //Create the file and write formatted scene to it
+            FileWriter fw = new FileWriter(file + ".scn",true);
+            fw.write(scene.toString());
+            fw.close();
+
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * Opens a file chooser interface in order to select the file
+     * that contains the required scene. Afterwards loads and returns
+     * the selected scene.
+     * @return Scene object that is contained in the selected file.
+     */
+    public Scene loadSceneWithMenu() {
+
+        //Open file chooser to load a new scene
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setCurrentDirectory(new File(System.getProperty("user.dir")));
+
+        switch (fileChooser.showOpenDialog(fileChooser)) {
+            case JFileChooser.APPROVE_OPTION: {
+
+                try {
+
+                    //Actual parsing and loading of file contents
+                    byte[] encoded = Files.readAllBytes(Paths.get(fileChooser.getSelectedFile().getPath()));
+                    return loadScene(new String(encoded, StandardCharsets.UTF_8));
+
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                break;
+            }
+            default: //Do nothing
+        }
+
+        return null;
+
+    }
+
+    /**
+     * Loads a Scene object from a given string input
+     * @param scene Encoded scene contents
+     * @return A scene object that is the parsed scene
+     */
+    public Scene loadScene(String scene) {
+        return new Scene(scene);
     }
 
 }
 
-class PolygonManager {
+class Scene {
 
     /**
-     * Returns all of the polygons stored in the manager.
+     * Creates an empty scene.
+     */
+    Scene() { }
+
+    /**
+     * Creates a scene from a formatted scene string.
+     * @param scene The formatted scene contents to load.
+     */
+    public Scene(String scene) {
+
+        java.util.List<Point> all_points = new java.util.ArrayList<>();
+        java.util.List<Point> all_edges = new java.util.ArrayList<>();
+
+        //Load all of the points and edges from the input
+        BufferedReader bufReader = new BufferedReader(new StringReader(scene));
+
+        String line;
+
+        /*
+         * The process is:
+         * 1) Read line that contains the number of points
+         * 2) Read all of the points
+         * 3) Read line that contains the number of edges
+         * 4) Read all of the edges
+         * 5) Combine all of the polygons via Scene methods
+         */
+
+        //Number of vertices
+        int limit = 0;
+
+        try {
+            limit = Integer.parseInt(bufReader.readLine());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for (int index = 0; index < limit; index++) {
+
+            try {
+
+                String[] raw_point = bufReader.readLine().split(" ");
+                int pos_x = Integer.parseInt(raw_point[0]);
+                int pos_y = Integer.parseInt(raw_point[1]);
+
+                //Extracted point
+                all_points.add(new Point(pos_x, pos_y));
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        //Number of edges
+        try {
+            limit = Integer.parseInt(bufReader.readLine());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for (int index = 0; index < limit; index++) {
+
+            try {
+
+                String[] raw_edge = bufReader.readLine().split(" ");
+                int start = Integer.parseInt(raw_edge[0]);
+                int end = Integer.parseInt(raw_edge[1]);
+
+                //Extracted point
+                all_edges.add(new Point(start, end));
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+
+        /*
+         * The edges contain instructions as to how to parse the
+         * polygon but the class can identify shape only by using
+         * setPolylineSection and endPolylineSection, as long as
+         * the final shape in contents is the polyline.
+         */
+        for (Point p : all_edges) {
+
+            //X contains the index of start point
+            setPolylineSection(all_points.get(p.x));
+            endPolylineSection();
+
+            //Y contains the index of end point
+            setPolylineSection(all_points.get(p.y));
+            endPolylineSection();
+
+        }
+
+    }
+
+    /**
+     * Returns all of the polygons stored in the scene.
      *
      * @return A list of polygons.
      */
@@ -234,6 +489,76 @@ class PolygonManager {
             //Second case - don't do anything
 
         }
+
+    }
+
+    /**
+     * Encodes the scene into a format.
+     * @return formatted scene string.
+     */
+    @Override
+    public String toString() {
+
+        String result = "";
+
+        /*
+         * Add polygon details to result for every one that exists,
+         * and add the edges to a separate array that stores them
+         */
+        java.util.List<Point> all_points = new java.util.ArrayList<>();
+        java.util.List<Point> all_edges = new java.util.ArrayList<>();
+        int edge_index = 0;
+
+        for (Polygon poly : getPolygons()) {
+
+            int points_x[] = poly.xpoints;
+            int points_y[] = poly.ypoints;
+
+            for (int index = 0 ; index < poly.npoints ; index++) {
+
+                //The current point that makes up the Polygon
+                all_points.add(new Point(points_x[index], points_y[index]));
+
+                /*
+                 * The Polygon is constructed from edges starting from 0 -> 1, 1 -> 2 ... n-1 -> n.
+                 * To make up this order we need to start from the second point and construct from first
+                 * and second point an edge (i.e. 0 -> 1), and continue in this fashion until the last point
+                 * (i.e. n-1 -> n). Afterwards update global edge count to avoid collision with former data.
+                 */
+                if (index != 0)
+                    all_edges.add(new Point(index + edge_index - 1, index + edge_index));
+
+            }
+
+            //Add the closing edge
+            all_edges.add(new Point(edge_index + poly.npoints - 1, edge_index));
+
+            edge_index += poly.npoints;
+
+        }
+
+        /*
+         * Now that we have all the data, return it as required in the format:
+         * # Vectices
+         * X0 Y0
+         * X1 Y1
+         * ...
+         *
+         * # Edges
+         * V0 V1
+         * ...
+         */
+        result += Integer.toString(all_points.size()) + '\n';
+
+        for (Point p : all_points)
+            result += Integer.toString(p.x) + ' ' + Integer.toString(p.y) + '\n';
+
+        result += Integer.toString(all_edges.size()) + '\n';
+
+        for (Point e : all_edges)
+            result += Integer.toString(e.x) + ' ' + Integer.toString(e.y) + '\n';
+
+        return result;
 
     }
 
